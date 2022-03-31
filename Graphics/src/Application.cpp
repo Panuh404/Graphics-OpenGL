44978888ce86b6cbdae9096fee03e374ba68174f
Panuh404 <gastonpannuto@gmail.com>
2022-03-31 19:17:25 -0300

    Commit 9: Camera Movement

--- Graphics/src/Application.cpp
+++ Graphics/src/Application.cpp
@@ -15,40 +15,51 @@
 #include "core/Renderer.h"
 #include "core/Texture.h"
 
+// Function Declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
+// Window Settings
+const uint32_t screenWidth = 800;
+const uint32_t screenHeight = 600;
 
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+// Camera Parameters
+glm::vec3 cameraPos =	glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp =	glm::vec3(0.0f, 1.0f,  0.0f);
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main() {
+	// Initialize and Configure GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Graphic Programming", NULL, NULL);
+	// GLFW Window Creation
+	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Graphic Programming", NULL, NULL);
 	if (!window) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
+	// load all OpenGL function pointers with glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	// Configure global opengl state
 	glEnable(GL_DEPTH_TEST);
-	
+
+	// Set up vertex data and configure vertex attributes
 	float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -107,6 +118,7 @@ int main() {
 	};
 	
 	Renderer renderer;
+	// build and compile shader
 	Shader shader("res/shaders/vertex_basic.shader", "res/shaders/fragment_basic.shader");
 
 	VertexBuffer VBO(vertices, sizeof(vertices));
@@ -115,13 +127,12 @@ int main() {
 	VBO.Bind();
 	VAO.Bind();
 	
-	// position attribute
+	// Position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);					
-	// texture attribute							
+	// Texture attribute							
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	
 	
 	// Texture Handling
 	Texture texture1("res/textures/container.jpg");
@@ -133,34 +144,38 @@ int main() {
 	
 	VBO.Unbind();
 	VAO.Unbind();
-	
+
+	// Pass Projection matrix to shader
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	shader.SetUniformMat4("projection", projection);
+
 	//Render Loop
 	while (!glfwWindowShouldClose(window)) {
+		// Per-frame time logic
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//Input
 		processInput(window);
 		
 		//Render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		renderer.Clear();
-		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
+		// Bind textures and activate shader
 		texture1.Bind(0);
 		texture2.Bind(1);
 		shader.Bind();
 		
-		//Create Transformations
-		glm::mat4 view			= glm::mat4(1.0f);
-		glm::mat4 projection	= glm::mat4(1.0f);
-		
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-		
+		// Camera/View Transformation
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		shader.SetUniformMat4("view", view);
-		shader.SetUniformMat4("projection", projection);
 		
 		VAO.Bind();
 		for (unsigned int i = 0; i < 10; i++) {
+			// Calculate the model matrix for each object and pass it to shader before drawing
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f * i;
@@ -170,11 +185,33 @@ int main() {
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 		
-		//Check and call events and swap the buffers
+		// Check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	
+	// Terminate, clearing all previously allocated GLFW resources
 	glfwTerminate();
 	return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	// Input Camera
+	float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
